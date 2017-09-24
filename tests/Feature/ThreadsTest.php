08c35df2f53ffe85f4ece862fcce7d9493defd43
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-24 01:03:01 -0400

    initial commit with basic tests

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_single_thread()
+    {
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_thread_replies()
+    {
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
