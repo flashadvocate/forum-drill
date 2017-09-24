6ad9365358336ec4edda2aab49b6d1f37fb689ef
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-24 01:26:08 -0400

    thread owner, tests for participation

---
+++ tests/Feature/ParticipatesInForumTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParticipatesInForumTest extends TestCase
+{
+    /** @test */
+    public function an_authed_user_may_participate_in_forum_threads()
+    {
+        // given an authed user
+        // and an existing thread
+        // whena us
+    }
+}
---
+++ tests/Unit/ThreadTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_thread_can_have_replies()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_has_a_creator()
+    {
+        $this->assertInstanceOf('App\User', $this->thread->creator);
+    }
+}
