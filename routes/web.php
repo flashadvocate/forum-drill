5395f7bcb821bd956f2b4aaf8bdd8f53d79d7db3
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-25 10:03:22 -0400

    implement thread creation tests

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index')->name('threads.index');
 Route::get('/threads/{thread}', 'ThreadsController@show')->name('threads.show');
 Route::post('/threads/{thread}/replies', 'RepliesController@store')->name('replies.store');
+Route::post('/threads', 'ThreadsController@store')->name('threads.store');
