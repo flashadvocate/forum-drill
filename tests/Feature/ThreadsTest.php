3a1545c056e126f267bf87071ef4a277fe466c29
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-25 22:31:43 -0400

    add testing helpers, refactor

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -15,7 +15,7 @@ class ReadThreadsTest extends TestCase
     {
         parent::setUp();
 
-        $this->thread = factory('App\Thread')->create();
+        $this->thread = create('App\Thread');
     }
 
     /** @test */
@@ -35,8 +35,7 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_view_thread_replies()
     {
-        $reply = factory('App\Reply')
-            ->create(['thread_id' => $this->thread->id]);
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
 
         $this->get($this->thread->path())
             ->assertSee($reply->body);
