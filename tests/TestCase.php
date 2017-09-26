83e6621f0582c4291ff39b7260ab79527b29982c
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-25 23:19:53 -0400

    omit deprecated handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->disableExceptionHandling();
+        $this->withoutExceptionHandling();
     }
 
     protected function signIn($user = null)
@@ -22,23 +22,6 @@ abstract class TestCase extends BaseTestCase
 
         $this->actingAs($user);
 
-        return $this;
-    }
-
-    protected function disableExceptionHandling()
-    {
-        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(\Exception $e) {}
-            public function render($request, \Exception $e) {
-                throw $e;
-            }
-        });
-    }
-    protected function withExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
         return $this;
     }
 }
